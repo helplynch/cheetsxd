1ce0c2be7bece22a79d2d8face1c74d597819ade
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-10 16:50:08 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,9 +1,12 @@
-local GamepassID = 269875453
-game.Players.PlayerAdded:Connect(function(player)
-	if game:GetService("MarketplaceService"):UserOwnsGamePassAsync(player.UserId, GamepassID) then
-		print("Loading Assware!")
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/ware.lua"))()
-	else
-		player:Kick("You don't have permission to use Assware, Please buy the gamepass and try again later.")
-	end
-end)
+local players = {
+	["lp0nda"] = true,
+	["FunnyNitron"] = true
+}
+local username = game.Players.LocalPlayer.Name
+
+if players[username] == true then
+	print("loading assware for " .. username)
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/ware.lua"))()
+else
+	game.Players.LocalPlayer:Kick("You dont have access to Assware, please open a ticket in the server if you bought the gamepass and need access")
+end
