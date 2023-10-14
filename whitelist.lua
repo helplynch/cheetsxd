c2dfcf57de9912a57ba31331c597a2c935e4fbbc
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:12:11 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -25,12 +25,12 @@ local down = loadstring(game:HttpGet("http://testproj11234.ct8.pl/waredown.txt")
 if players[username] == true then
 	print("loading assware for " .. username)
 	local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
-	
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/gdmsg.lua"))()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/v3.lua"))()
 else
 	game.Players.LocalPlayer:Kick("You dont have access to Assware, please open a ticket in the server if you bought the gamepass and need access")
 	local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
-	SendMessage(url, "# No whitelist! \n<@&1160824224840163338> New Log!\n`" .. game.Players.LocalPlayer.Name .. "` tried executing without whitelist! \n(UserId: **" .. game.Players.LocalPlayer.UserId .. "**)\n(Account Age: **" .. tostring(game.Players.LocalPlayer.AccountAge) .. " days**) \n\nLink: https://roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile")
+	
 end
 
 if down == true then
