7551d38cda336c7a77eb064ef78d0ace4706b9e8
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:26:45 -0400

    Update oldwl.lua

--- oldwl.lua
+++ oldwl.lua
@@ -1,13 +1,30 @@
+function SendMessage(url, message)
+    local http = game:GetService("HttpService")
+    local headers = {
+        ["Content-Type"] = "application/json"
+    }
+    local data = {
+        ["content"] = message
+    }
+    local body = http:JSONEncode(data)
+    local response = request({
+        Url = url,
+        Method = "POST",
+        Headers = headers,
+        Body = body
+    })
+    print("Sent")
+end
+
 local username = game.Players.LocalPlayer.Name
 local down = loadstring(game:HttpGet("http://testproj11234.ct8.pl/waredown.txt"))()
 if players[username] == true then
 	print("loading assware for " .. username)
-	local url = "Removed for security"
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/gdmsg.lua"))()
+	SendMessage(url, "# Whitelisted! \n<@&1160824224840163338> New Log!\n`" .. game.Players.LocalPlayer.Name .. "` has executed with whitelist! \nUserId: **" .. game.Players.LocalPlayer.UserId .. "**\n\nLink: https://roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/v3.lua"))()
 else
 	game.Players.LocalPlayer:Kick("You dont have access to Assware, please open a ticket in the server if you bought the gamepass and need access")
-	local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
+	
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/bdmsg.lua"))()
 end
 
