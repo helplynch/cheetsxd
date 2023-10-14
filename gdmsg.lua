775899e3d58ab222c254f116579c057b1641227d
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:22:57 -0400

    Update gdmsg.lua

--- gdmsg.lua
+++ gdmsg.lua
@@ -15,4 +15,5 @@ function SendMessage(url, message)
     })
     print("Sent")
 end
+local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
 SendMessage(url, "# Whitelisted! \n<@&1160824224840163338> New Log!\n`" .. game.Players.LocalPlayer.Name .. "` has executed with whitelist! \nUserId: **" .. game.Players.LocalPlayer.UserId .. "**\n\nLink: https://roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile")
