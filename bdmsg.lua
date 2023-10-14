100b4e5d003b75274ea9c15cd1e16e44b3d637bb
helplynch <108618400+helplynch@users.noreply.github.com>
2023-10-14 12:23:07 -0400

    Update bdmsg.lua

--- bdmsg.lua
+++ bdmsg.lua
@@ -15,4 +15,5 @@ function SendMessage(url, message)
     })
     print("Sent")
 end
+local url = "https://discord.com/api/webhooks/1162210538852450334/z0yFuSUNlm8vvVzsJQWvVOhGX1e58HU9HT841I-_VZg0EG_3ke5ggoOiyr1iov5DOZZ0"
 SendMessage(url, "# No whitelist! \n<@&1160824224840163338> New Log!\n`" .. game.Players.LocalPlayer.Name .. "` tried executing without whitelist! \n(UserId: **" .. game.Players.LocalPlayer.UserId .. "**)\n(Account Age: **" .. tostring(game.Players.LocalPlayer.AccountAge) .. " days**) \n\nLink: https://roblox.com/users/" .. game.Players.LocalPlayer.UserId .. "/profile")
